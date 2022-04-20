092db0afaeb943aebe9c6a554860c8a6bb505c5f
peterrevsbech <peter.revsbech@gmail.com>
2022-04-20 23:32:34 +0200

    Made string parsing work with and without parentheses + use operator precedence

--- src/RevisionAgent.java
+++ src/RevisionAgent.java
@@ -16,7 +16,7 @@ public class RevisionAgent {
 
             Proposition proposition;
             try {
-                proposition = toProposition(input);
+                proposition = StringConverter.toProposition(input);
                 System.out.println("Recieved proposition:");
                 System.out.println(proposition);
             } catch (Exception e){
@@ -40,121 +40,5 @@ public class RevisionAgent {
     }
 
 
-    public static Proposition toProposition(String prop){
-        //Strip of whitespace
-        prop = prop.replaceAll("\\s", "");
 
-        //Perform conversion
-        Proposition proposition = toPropositionInner(prop);
-
-        //Set parents in whole tree
-        proposition.setParents(null);
-
-        return proposition;
-    }
-
-
-
-    private static Proposition toPropositionInner(String prop){
-        Proposition p1, p2;
-
-        if (prop.contains("(")){
-            boolean  findp1 = true;
-            boolean b = false; //indicates if top connective is binary (As opposed to unary)
-            int counter = 0;
-            int startp1 = 0;
-            int endp1 = 0;
-            int startp2 = 0;
-            int endp2 = 0;
-
-            String partTwo;
-            for (int i = 0; i < prop.length(); i++){
-                if(prop.charAt(i) == '('){
-                    if(!b){
-                        b = true;
-                        if(findp1) {
-                            startp1 = i;
-                        }else{
-                            startp2 = i;
-                        }
-                    }
-                    counter ++;
-                }
-                else if(prop.charAt(i) == ')'){
-                    counter --;
-                }
-                if (b && counter == 0){
-                    if(findp1) {
-                        endp1 = i;
-                        findp1 = false;
-                        b =false;
-                    }else{
-                        endp2 = i;
-                        break;
-                    }
-                }
-            }
-
-            p1 = toPropositionInner((String) prop.subSequence(startp1+1,endp1));
-
-            p2 = null;
-            if(b) {
-                p2 = toPropositionInner((String) prop.subSequence(startp2 + 1, endp2 - 1));
-            }
-
-            String m = prop.substring(endp1+1,startp2-1);
-            Proposition p = null;
-            if(m.contains(Dict.AND)){
-                p = new And(null,p1, p2);
-            } else if (m.contains(Dict.BIIMP)){
-                p = new BiImplication(null,p1,p2);
-            } else if (m.contains(Dict.IMP)){
-                p = new Implication(null,p1,p2);
-            } else if (m.contains(Dict.OR)){
-                p = new Or(null,p1,p2);
-            } else if (!b && prop.substring(0,startp1-1).contains(Dict.NOT)){
-                p =  new Not(null,p1);
-            }
-            return p;
-        }
-        //If there were not parentheses
-        return noParenthesesToProp(prop);
-    }
-
-    private static Proposition noParenthesesToProp(String prop){
-        //Prop has no parantheses, e.g. "A&B", "C", "A&B => C|D"
-        //Split on highest level operator: hierarchy is BIIMP, IMP, OR, AND, NOT
-        String[] args;
-        Proposition p1, p2;
-        if (prop.contains(Dict.BIIMP)){
-            args = prop.split(Dict.BIIMP,2);
-            p1 = noParenthesesToProp(args[0]);
-            p2 = noParenthesesToProp(args[1]);
-            return new BiImplication(null, p1, p2);
-        } else if (prop.contains(Dict.IMP)){
-            args = prop.split(Dict.IMP,2);
-            p1 = noParenthesesToProp(args[0]);
-            p2 = noParenthesesToProp(args[1]);
-            return new Implication(null, p1, p2);
-        } else if (prop.contains(Dict.OR)){
-            args = prop.split(Dict.OR,2);
-            p1 = noParenthesesToProp(args[0]);
-            p2 = noParenthesesToProp(args[1]);
-            return new Or(null, p1, p2);
-        } else if (prop.contains(Dict.AND)){
-            args = prop.split(Dict.AND,2);
-            p1 = noParenthesesToProp(args[0]);
-            p2 = noParenthesesToProp(args[1]);
-            return new And(null, p1, p2);
-        } else if (prop.contains(Dict.NOT)){
-            //Must only contain NOT LITERAL
-            String var = prop.replace(Dict.NOT,"");
-            p1 = new Literal(null,var);
-            return new Not(null,p1);
-        } else {
-            //Must be literal only
-            Literal l = new Literal(null,prop);
-            return l;
-        }
-    }
 }
---
+++ src/StringConverter.java
@@ -0,0 +1,162 @@
+public class StringConverter {
+    private static String operatorString;
+    public static Proposition toProposition(String prop){
+        //Strip of whitespace
+        prop = prop.replaceAll("\\s", "");
+
+        //Perform conversion
+        Proposition proposition = toPropositionInner(prop);
+
+        //Set parents in whole tree
+        proposition.setParents(null);
+
+        return proposition;
+    }
+
+
+
+    private static Proposition toPropositionInner(String prop){
+        //Find top level operator
+        int index = findTopLevelOperatorIndex(prop);
+
+        if (index == -1){
+            //If no top level operator was found
+            if (prop.contains("(")){
+                //Must be redundant parenthesis
+                return toPropositionInner(prop.substring(1,prop.length()-1));
+            } else {
+                //Can evaluate without parenthes
+                return noParenthesesToProp(prop);
+            }
+        }
+
+        //If a top level operator was found
+        String p1, p2;
+        int p2startIndex = index + operatorString.length();
+        p1 = prop.substring(0,index);
+        p2 = prop.substring(p2startIndex);
+        if (operatorString.equals(Dict.BIIMP)){
+            return new BiImplication(null,toPropositionInner(p1),toPropositionInner(p2));
+        } else if (operatorString.equals(Dict.IMP)){
+            return new Implication(null,toPropositionInner(p1),toPropositionInner(p2));
+        } else if (operatorString.equals(Dict.OR)){
+            return new Or(null,toPropositionInner(p1),toPropositionInner(p2));
+        } else if (operatorString.equals(Dict.AND)){
+            return new And(null,toPropositionInner(p1),toPropositionInner(p2));
+        } else if (operatorString.equals(Dict.NOT)){
+            return new Not(null,toPropositionInner(p2));
+        }
+        System.out.println("Error in toPropositionInner()");
+        return null;
+    }
+
+    /**
+     * Returns start index of the top level parameter of the string.
+     * E.g. for "(A => B) v (C & D)" returns index of 'v'
+     * For "A v B <=> C & D" returns index of '<' in "<=>"
+     * For "( A v B )" returns -1 since there is no top level operator
+     *
+     * Also sets value of operatorString to actual string for the operator
+     * @param prop
+     * @return
+     */
+    private static int findTopLevelOperatorIndex(String prop){
+        int openParenthesisCount = 0;
+        int bestIndex = -1;
+        int operatorPriority = -1;
+        String c;
+        for (int i = 0; i < prop.length(); i++) {
+            c = prop.substring(i,i+1);
+            if (c.equals("(")){
+                openParenthesisCount++;
+            } else if (c.equals(")")){
+                openParenthesisCount--;
+            }
+
+            //Only consider the other symbols, if we are not inside parenthesis
+            if (openParenthesisCount == 0){
+
+                if (operatorStartsAt(i,prop,Dict.BIIMP)){
+                    if (operatorPriority < 5){
+                        operatorPriority = 5;
+                        bestIndex = i;
+                        operatorString = Dict.BIIMP;
+                    }
+                } else if (operatorStartsAt(i,prop,Dict.IMP)){
+                    if (operatorPriority < 4){
+                        operatorPriority = 4;
+                        bestIndex = i;
+                        operatorString = Dict.IMP;
+                    }
+                } else if (operatorStartsAt(i,prop,Dict.OR)){
+                    if (operatorPriority < 3){
+                        operatorPriority = 3;
+                        bestIndex = i;
+                        operatorString = Dict.OR;
+                    }
+                } else if (operatorStartsAt(i,prop,Dict.AND)){
+                    if (operatorPriority < 2){
+                        operatorPriority = 2;
+                        bestIndex = i;
+                        operatorString = Dict.AND;
+                    }
+                } else if (operatorStartsAt(i,prop,Dict.NOT)){
+                    if (operatorPriority < 1){
+                        operatorPriority = 1;
+                        bestIndex = i;
+                        operatorString = Dict.NOT;
+                    }
+
+                }
+            }
+        }
+        if (operatorString != null){};//redundant line, but otherwise compiler ignores variable
+        return bestIndex;
+    }
+
+    private static boolean operatorStartsAt(int index, String prop, String operator){
+        int len = operator.length();
+        if (index+len-1 < prop.length()){
+            String operatorSubString = prop.substring(index,index+len);
+            return operator.equals(operatorSubString);
+        }
+        return false;
+    }
+
+    private static Proposition noParenthesesToProp(String prop){
+        //Prop has no parantheses, e.g. "A&B", "C", "A&B => C|D"
+        //Split on highest level operator: hierarchy is BIIMP, IMP, OR, AND, NOT
+        String[] args;
+        Proposition p1, p2;
+        if (prop.contains(Dict.BIIMP)){
+            args = prop.split(Dict.BIIMP,2);
+            p1 = noParenthesesToProp(args[0]);
+            p2 = noParenthesesToProp(args[1]);
+            return new BiImplication(null, p1, p2);
+        } else if (prop.contains(Dict.IMP)){
+            args = prop.split(Dict.IMP,2);
+            p1 = noParenthesesToProp(args[0]);
+            p2 = noParenthesesToProp(args[1]);
+            return new Implication(null, p1, p2);
+        } else if (prop.contains(Dict.OR)){
+            args = prop.split(Dict.OR,2);
+            p1 = noParenthesesToProp(args[0]);
+            p2 = noParenthesesToProp(args[1]);
+            return new Or(null, p1, p2);
+        } else if (prop.contains(Dict.AND)){
+            args = prop.split(Dict.AND,2);
+            p1 = noParenthesesToProp(args[0]);
+            p2 = noParenthesesToProp(args[1]);
+            return new And(null, p1, p2);
+        } else if (prop.contains(Dict.NOT)){
+            //Must only contain NOT LITERAL
+            String var = prop.replace(Dict.NOT,"");
+            p1 = new Literal(null,var);
+            return new Not(null,p1);
+        } else {
+            //Must be literal only
+            Literal l = new Literal(null,prop);
+            return l;
+        }
+    }
+}
