8f76a809948a0c6f32a94a4e50bb64585b667bc6
MrGundulf <48431504+MrGundulf@users.noreply.github.com>
2022-04-21 19:36:19 +0200

    CNF done, needs test

--- src/CNF.java
+++ src/CNF.java
@@ -2,11 +2,13 @@ public class CNF {
 
 
     public Proposition toCNF(Proposition proposition){
+        Proposition proposition1 = eliminate(proposition);
+        Proposition proposition2 = demorgan(proposition1);
 
         return null;
     }
 
-    public Proposition eliminate(Proposition proposition){
+    private Proposition eliminate(Proposition proposition){
         if (proposition instanceof Not ){
             Proposition proposition1 = eliminate(proposition.A);
             Proposition proposition2 = new Not( null,proposition1);
@@ -62,5 +64,88 @@ public class CNF {
         return null;
     }
 
+    private Proposition demorgan(Proposition proposition){
+        if (proposition instanceof Not && proposition.A instanceof And) {
+            Proposition proposition1 = new Not(null, proposition.A.A);
+            Proposition proposition2 = new Not(null, ((And) proposition.A).B);
+            Proposition proposition3 = demorgan(proposition1);
+            Proposition proposition4 = demorgan(proposition2);
+            Proposition proposition5 = new Or(null, proposition3, proposition4);
+            proposition3.parent = proposition5;
+            proposition4.parent = proposition5;
+            return proposition5;
+        }else if (proposition instanceof Not && proposition.A instanceof Or){
+            Proposition proposition1 = new Not(null, proposition.A.A);
+            Proposition proposition2 = new Not(null, ((And) proposition.A).B);
+            Proposition proposition3 = demorgan(proposition1);
+            Proposition proposition4 = demorgan(proposition2);
+            Proposition proposition5 = new And(null, proposition3, proposition4);
+            proposition3.parent = proposition5;
+            proposition4.parent = proposition5;
+            return proposition5;
+        }else if (proposition instanceof And){
+            Proposition proposition1 = demorgan(proposition.A);
+            Proposition proposition2 = demorgan(((And) proposition).B);
+            Proposition proposition3 = new And(null,proposition1,proposition2);
+            proposition1.parent = proposition3;
+            proposition2.parent = proposition3;
+            return proposition3;
+        }else if (proposition instanceof Or){
+            Proposition proposition1 = demorgan(proposition.A);
+            Proposition proposition2 = demorgan(((And) proposition).B);
+            Proposition proposition3 = new Or(null,proposition1,proposition2);
+            proposition1.parent = proposition3;
+            proposition2.parent = proposition3;
+            return proposition3;
+        }else if (proposition instanceof Not && proposition.A instanceof Not){
+            Proposition proposition1 = demorgan(proposition.A.A);
+            return  proposition1;
+        }else  if (proposition instanceof Literal){
+            return proposition;
+        }
+        return null;
+    }
+
+    private Proposition distribute(Proposition proposition){
+        if(proposition instanceof And){
+            Proposition proposition1 = distribute(proposition.A);
+            Proposition proposition2 = distribute(((And) proposition).B);
+            Proposition proposition3 = new And(null,proposition1,proposition2);
+            proposition1.parent = proposition3;
+            proposition2.parent = proposition2;
+            return proposition3;
+        } else if (proposition instanceof Or){
+            Proposition proposition1 = distribute(proposition.A);
+            Proposition proposition2 = distribute(((Or) proposition).B);
+            Proposition proposition3 = distribute(proposition1,proposition2);
+            return  proposition3;
+        }else if (proposition instanceof Literal){
+            return proposition;
+        }
+        return null;
+    }
+
+    private  Proposition distribute(Proposition proposition1,Proposition proposition2){
+        if (proposition1 instanceof And){
+            Proposition proposition3 = distribute(proposition1.A,proposition2);
+            Proposition proposition4 = distribute(((And) proposition1).B,proposition2);
+            Proposition proposition = new And(null,proposition3,proposition4);
+            proposition3.parent = proposition;
+            proposition4.parent = proposition;
+            return proposition;
+        } else  if (proposition2 instanceof And) {
+            Proposition proposition3 = distribute(proposition1, proposition2.A);
+            Proposition proposition4 = distribute( proposition1, ((And) proposition2).B);
+            Proposition proposition = new And(null, proposition3, proposition4);
+            proposition3.parent = proposition;
+            proposition4.parent = proposition;
+            return proposition;
+        }else{
+            Proposition proposition = new Or(null,proposition1,proposition2);
+            proposition1.parent = proposition;
+            proposition2.parent = proposition;
+            return  proposition;
+        }
+    }
 
 }
