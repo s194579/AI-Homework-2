b662d99fcb244c211b6dc33a0ef3ba236188e2eb
peterrevsbech <peter.revsbech@gmail.com>
2022-04-26 12:01:10 +0200

    Finishing DPLL - not finished yet

--- src/SAT.java
+++ src/SAT.java
@@ -40,9 +40,66 @@ public class SAT {
             return true;
         }
 
-        //TODO add these optimizations
         // Find eventual pure symbols and simplify
+        // If symbol only has positive sign in unresolved clauses then it is pure
+
         // Find eventual unit clauses and simplify
+        // If there is only one symbol in clause with value "unknown" then it is unit clause
+        HashMap<String,Boolean> pureSymbolMap = new HashMap<String, Boolean>();
+        for (Proposition clause: clauses) {
+            boolean isResolved = Model.value.T==clause.truthValue(model);
+            if (isResolved){ //Ignore clauses that are already resolved
+                continue;
+            }
+
+            List<Proposition> literals = clauseToPropList(clause);
+            for (Proposition literal: literals) {
+                int numOfUnknownLiterals = 0;
+                String latestUnkownSymbol = null;
+                Model.value latestUnkownSymbolValue = Model.value.unknown;
+                if (literal instanceof Literal){ //If it is not a Not(Literal)
+                    // Check if already found in negated form
+                    String symbol = ((Literal) literal).var;
+                    Boolean val = pureSymbolMap.get(symbol);
+                    if (model.modelValues.get(symbol)== Model.value.unknown){
+                        numOfUnknownLiterals++;
+                        latestUnkownSymbol = symbol;
+                        latestUnkownSymbolValue = Model.value.T;
+                    }
+
+                    if (val == null){
+                        pureSymbolMap.put(symbol,true);
+                    }
+                } else if (literal instanceof Not && literal.A instanceof Literal){
+                    String symbol = ((Literal) literal.A).var;
+                    Boolean val = pureSymbolMap.get(symbol);
+                    if (val == true || val == null){
+                        pureSymbolMap.put(symbol,false);
+                    }
+                    if (model.modelValues.get(symbol)== Model.value.unknown){
+                        numOfUnknownLiterals++;
+                        latestUnkownSymbol = symbol;
+                        latestUnkownSymbolValue = Model.value.F;
+                    }
+                }
+
+                // If unit clause then assign approriate value
+                if (numOfUnknownLiterals == 1){
+                    model.modelValues.put(latestUnkownSymbol,latestUnkownSymbolValue);
+                }
+            }
+        }
+
+        // Find the pure symbols and assign value to true
+        for (String symbol: pureSymbolMap.keySet()) {
+            Boolean val = pureSymbolMap.get(symbol);
+            if (val == true){
+                model.modelValues.put(symbol, Model.value.T);
+            }
+        }
+
+
+
 
         // Assume values of first symbol and recurse
         String unknownSymbol = model.getFirstUnknownSymbol();
@@ -92,6 +149,20 @@ public class SAT {
         }
 
         return list;
+    }
+
+    private static List<Proposition> clauseToPropList(Proposition prop){
+        List<Proposition> props = new ArrayList<>();
+        if (prop instanceof Literal || prop instanceof Not && prop.A instanceof Literal){
+            props.add(prop);
+            return props;
+        }
+
+        props.addAll(clauseToPropList(prop.A));
+        if (prop instanceof Or){
+            props.addAll(clauseToPropList(((Or)prop).B));
+        }
+        return props;
     }
 
     static class Unit{
