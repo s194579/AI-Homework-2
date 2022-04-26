44c7b9adde7fbce4909c6b80ec55e6592a639184
peterrevsbech <peter.revsbech@gmail.com>
2022-04-26 22:44:24 +0200

    Deleted unused code

--- src/Application.java
+++ src/Application.java
@@ -1,37 +1,6 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
-
-
 public class Application {
     public static void main(String[] args) {
         RevisionAgent agent = new RevisionAgent();
         agent.start();
-    }
-
-    public static List<int[]> generate(int n, int r) {
-        List<int[]> combinations = new ArrayList<>();
-        int[] combination = new int[r];
-
-        // initialize with lowest lexicographic combination
-        for (int i = 0; i < r; i++) {
-            combination[i] = i;
-        }
-
-        while (combination[r - 1] < n) {
-            combinations.add(combination.clone());
-
-            // generate next combination in lexicographic order
-            int t = r - 1;
-            while (t != 0 && combination[t] == n - r + t) {
-                t--;
-            }
-            combination[t]++;
-            for (int i = t + 1; i < r; i++) {
-                combination[i] = combination[i - 1] + 1;
-            }
-        }
-
-        return combinations;
     }
 }
